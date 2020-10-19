1c2cdb3a1f090d5919efc95b887d0a05a2a3511e
imthevk <krvijay878@gmail.com>
2020-10-19 21:48:44 +0530

    build app

---
+++ build/precache-manifest.7b9f0fb58bfab3c515a8408c6f23c6e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8d5eddb4d9e117c910c3fc9a6dd668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae32f529d44e8077b5d",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7d27510b96428b190a0c",
+    "url": "/static/js/2.0bbba42d.chunk.js"
+  },
+  {
+    "revision": "fae32f529d44e8077b5d",
+    "url": "/static/js/main.72eebdd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
